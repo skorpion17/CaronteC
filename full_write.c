145b36680c6878c1332a88f4fcbc67331b434498
Andrea Mayer <mayer.andrea.17@gmail.com>
2016-02-16 18:03:29 +0100

    Aggiornati i commenti su full_write.c.

--- full_write.c
+++ full_write.c
@@ -7,21 +7,32 @@
 
 #include "full_write.h"
 
+/**
+ * Implementazione della write per scrivere sulla socket il cui
+ * descrittore è identificato da fd, il buffer di lettura è buf
+ * e la quantità totale di byte da leggere è pari a count.
+ */
 ssize_t full_write(int fd, const void *buf, size_t count) {
 	size_t nleft;
 	ssize_t nwritten;
 
 	nleft = count;
-	while (nleft > 0) { /* repeat until no left */
+	/* Ripeti fino a quando ci sono byte da leggere. */
+	while (nleft > 0) {
 		if ((nwritten = write(fd, buf, nleft)) < 0) {
-			if (errno == EINTR) { /* if interrupted by system call */
+			if (errno == EINTR) {
+				/* Nel caso in cui ci sia una interruzione da syscall, ripeti il loop.*/
 				continue; /* repeat the loop */
 			} else {
-				return (nwritten); /* otherwise exit with error */
+				/**
+				 * Per tutti gli altri messaggi di errore si interrompe la lettura
+				 * e si ritorna il numero di byte letti.
+				 */
+				return (nwritten);
 			}
 		}
-		nleft -= nwritten; /* set left to write */
-		buf += nwritten; /* set pointer */
+		nleft -= nwritten;
+		buf += nwritten;
 	}
 	return (nleft);
 }
